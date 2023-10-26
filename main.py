cff5061c006b0f231d3f1738bf7af8bd7593cdb8
cj-casey <111756500+cj-casey@users.noreply.github.com>
2023-10-26 13:05:11 -0400

    main.py
    forgot commenting

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import pygame
 import os
 import math
 import random
+#TANK TUSSLE BY CONNOR CASEY 1/9/2023
+#PYGAME INITIALIZATION 
 pygame.font.init()
 pygame.mixer.init()
+
+#CONSTANT DECLARATIONS (THERE'S A LOT)
 WIDTH, HEIGHT = 900, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tank Tussle")
@@ -22,17 +26,22 @@ BLUE = (0,0,255)
 GREEN_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 MAX_BULLETS = 5
+
+#Object Declaration
 MENU_MUSIC = pygame.mixer.Sound(os.path.join("assets","TankTussle_Music.wav"))
 SWITCH = pygame.mixer.Sound(os.path.join("assets","switching.wav"))
 SELECT = pygame.mixer.Sound(os.path.join("assets","selecting.wav"))
 ROTATE = pygame.mixer.Sound(os.path.join("assets",'rotating.wav'))
 MOVE = pygame.mixer.Sound(os.path.join("assets",'moving.wav'))
 BOOM = pygame.mixer.Sound(os.path.join("assets",'explosion.wav'))
-
+#Image Obtainment
 GREEN_IMAGE = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join('assets','i01_green.png')),(PLAYER_WIDTH,PLAYER_HEIGHT)),-90)
 RED_IMAGE = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join('assets','i02_red.png')),(PLAYER_WIDTH,PLAYER_HEIGHT)),90)
 LOGO = pygame.transform.scale(pygame.image.load(os.path.join('assets','logo.png')),(800,200))
 FONT = pygame.font.Font(os.path.join('assets','PublicPixel.ttf'),80)
+
+#Class Constructors
+
 class wall_class():
     def __init__(self,type,x,y):
         self.type = type
@@ -50,6 +59,9 @@ class wallbreaker_class():
         self.shot = pygame.Rect(x + PLAYER_WIDTH, y + PLAYER_HEIGHT // 2 - 2, 10, 5)
         self.angle = angle
         self.color = color
+
+
+#Menu function, responsible for drawing the entire menu
 def draw_menu(menu_index):
     WIN.fill(GRAY)
     logo = pygame.Rect(50,100,800,100)
@@ -68,6 +80,9 @@ def draw_menu(menu_index):
     WIN.blit(play_text,(play_button.x+50,play_button.y+5))
     WIN.blit(quit_text,(quit_button.x+50,quit_button.y+5))
     pygame.display.update()
+
+#The bread and butter, responsible for drawing all of the objects on the screen
+#Iterates through all lists of objects and draws respective shapes
 def draw_window(green,red,green_bullets,red_bullets,wallbreakers,red_angle,green_angle,walls):
     WIN.fill(WHITE)
     for bullet in green_bullets:
@@ -86,6 +101,7 @@ def draw_window(green,red,green_bullets,red_bullets,wallbreakers,red_angle,green
     WIN.blit(red_sprite, (rect2.x, rect2.y))
     pygame.display.update()
     return green,red
+ #Ending Screen function, draws the end screen and allows the user to replay   
 def draw_winner(text):
     menu = True
     replay = False
@@ -129,6 +145,7 @@ def draw_winner(text):
         WIN.blit(quit_text, (quit_button.x + 50, quit_button.y + 5))
         pygame.display.update()
     return replay
+#This generates the grid based maze, it is just RNG. As a solution to getting blocked in by walls, I created the Wall_Breaker Class
 def grid_creation(walls):
     x = -2
     y = 0
@@ -154,10 +171,14 @@ def grid_creation(walls):
 
 
     return walls
+
+#Responsible for turning the image when the user turns, most high level code in the whole game
 def rot_center(image, angle, x, y):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
     return rotated_image, new_rect
+
+#Responsible for calculating the angles of bullets upon colliding with a wall, it's a little buggy
 def angle_calculation(angle,type,):
     if angle < 0:
         angle += 360
@@ -199,11 +220,15 @@ def angle_calculation(angle,type,):
         elif angle == 90:
             angle = 270
     return angle
+
+#classic distance equation
 def distance(ax,ay,bx,by):
     deltax = math.fabs(ax-bx)
     deltay = math.fabs(ay-by)
     distance = math.ceil(math.sqrt(pow(deltax,2)+pow(deltay,2)))
     return distance
+
+#wall collision detection, insanely bad, need to fix this
 def wall_collision(vel_x,vel_y,hitbox,walls,color,direction,angle,bullet1):
     type = ""
     collide = False
@@ -233,6 +258,7 @@ def wall_collision(vel_x,vel_y,hitbox,walls,color,direction,angle,bullet1):
                 collide = True
                 type = wall.type
     return collide,type
+#Movement for player 1, detects key inputs and changes the xy coordinates of the green tank
 def player1_handle_movement(keys_pressed,green_angle,green,walls,hitbox,rotate,moving):
     if keys_pressed[pygame.K_a]:  # LEFT
         green_angle += TURNING_SPEED
@@ -259,6 +285,8 @@ def player1_handle_movement(keys_pressed,green_angle,green,walls,hitbox,rotate,m
             green.y += y
             green.x -= x
     return green_angle,rotate,moving
+
+#same as player 1 but its red
 def player2_handle_movement(keys_pressed,red_angle,red,walls,hitbox,rotate,moving):
     if keys_pressed[pygame.K_LEFT]:  # LEFT
         red_angle += TURNING_SPEED
@@ -285,6 +313,8 @@ def player2_handle_movement(keys_pressed,red_angle,red,walls,hitbox,rotate,movin
             red.y += y
             red.x -= x
     return red_angle,rotate,moving
+
+#Handles all bullet movements, iterates through the vectors and changes their xy coordinates by the constant BULLET_VEL
 def handle_bullets(green_bullets, red_bullets, green, red,wallbreakers,walls):
     for bullet in green_bullets:
         x,y = x_y_calculation(bullet.angle,BULLET_VEL,"bullet")
@@ -322,12 +352,16 @@ def handle_bullets(green_bullets, red_bullets, green, red,wallbreakers,walls):
             if wall.pos.colliderect(wallbreaker.shot):
                 walls.remove(wall)
                 wallbreakers.remove(wallbreaker)
+
+#Counter that increments on collisions
 def handle_ricochets(bullets,walls,color):
     for bullet in bullets:
         collision,wall_type = wall_collision(1,1,bullet.shot,walls,color,"forwards",bullet.angle,"bullet")
         if collision:
                 bullet.angle = angle_calculation(bullet.angle,wall_type)
                 bullet.bounces -= 1
+
+#trig!!! Yay!! Just simple trig to create components of the vectors responsible for movement
 def x_y_calculation(angle,velocity,type):
     x = (velocity*math.fabs(math.cos(math.radians(angle))))
     y = (velocity*math.fabs(math.sin(math.radians(angle))))
@@ -364,6 +398,7 @@ def x_y_calculation(angle,velocity,type):
         y = -1*y
         x = x
     return x,y
+#defines the starting coordinates for bullets, a little buggy
 def bullet_start_coords(color,angle):
     radius = PLAYER_WIDTH//2
     y = 0
@@ -406,12 +441,15 @@ def bullet_start_coords(color,angle):
     if color == "red":
         x = -1*x
     return x,y
+
+#Main! Very bloated but it works.
 def main():
     clock = pygame.time.Clock()
     menu_run = True
     menuIndex = 0
     menu = ["play", "quit"]
     MENU_MUSIC.play()
+    #menu loop
     while menu_run:
         clock.tick(FPS)
         draw_menu(menuIndex)
@@ -437,7 +475,9 @@ def main():
                         MENU_MUSIC.stop()
                         pygame.quit()
     replay = True
+    #game loop
     while replay:
+        #variable declaration
         replay = False
         green = pygame.Rect(50.0, 50.0, PLAYER_WIDTH, PLAYER_HEIGHT)
         red = pygame.Rect(850.0, 850.0, PLAYER_WIDTH, PLAYER_HEIGHT)
@@ -460,32 +500,38 @@ def main():
         walls = []
         walls = grid_creation(walls)
         run = True
+        #run loop
         while run:
             clock.tick(FPS)
+            #checks through pygame events to determine behaviors
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LSHIFT and len(green_bullets) < MAX_BULLETS:
+                        #fires bullets for green
                         x,y = bullet_start_coords(green,green_angle)
                         bullet_class(3,5,1)
                         bullet = bullet_class(green_angle, (green.x - PLAYER_WIDTH) + x, (green.y-PLAYER_WIDTH//2)+y)
                         green_bullets.append(bullet)
                     if event.key == pygame.K_LCTRL and green_wallbreakers > 0:
+                        #fires wallbreakers for green
                         x,y = bullet_start_coords(green,green_angle)
                         wallbreaker = wallbreaker_class(green_angle, (green.x - PLAYER_WIDTH) + x, (green.y-PLAYER_WIDTH//2)+y,"green")
                         wallbreaker_list.append(wallbreaker)
                         green_wallbreakers -= 1
                     if event.key == pygame.K_RSHIFT and len(red_bullets) < MAX_BULLETS:
+                        #fires bullets for red
                         x, y = bullet_start_coords("red", red_angle)
-
                         bullet = bullet_class(red_angle, red.x - PLAYER_WIDTH + x, (red.y-PLAYER_WIDTH//2)-y,)
                         red_bullets.append(bullet)
                     if event.key == pygame.K_m and red_wallbreakers > 0:
+                        #fires wallbreakers for red
                         x,y = bullet_start_coords("red",red_angle)
                         wallbreaker = wallbreaker_class(red_angle, (red.x - PLAYER_WIDTH) + x, (red.y - PLAYER_WIDTH // 2) + y,"red")
                         wallbreaker_list.append(wallbreaker)
                         red_wallbreakers -= 1
+                #turning events
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_a and g_rotate:
                         ROTATE.stop()
@@ -503,17 +549,18 @@ def main():
                         MOVE.stop()
                     if event.key == pygame.K_UP and r_move:
                         MOVE.stop()
-
+                #kills the tanks if hit
                 if event.type == GREEN_HIT:
                     green_health -= 1
 
                 if event.type == RED_HIT:
                     red_health -= 1
-
+                #sets winner
                 if red_health <= 0:
                     winner_text = "Green Wins!"
                 if green_health <= 0:
                     winner_text = "Red Wins!"
+                #calls the end screen
                 if winner_text != "":
                     replay = draw_winner(winner_text)
                     if replay == False:
@@ -521,7 +568,7 @@ def main():
                         run = False
                     if replay == True:
                         run = False
-                    # SOMEONE WON
+            #all functions called no matter what, it also resets the value for degrees if they go over 360/-360            
             keys_pressed = pygame.key.get_pressed()
             green_angle,g_rotate,g_move = player1_handle_movement(keys_pressed,green_angle, green,walls,grn_hitbox,g_rotate,g_move)
             red_angle,r_rotate,r_move = player2_handle_movement(keys_pressed, red_angle, red,walls,red_hitbox,r_rotate,r_move)
@@ -533,8 +580,6 @@ def main():
             handle_ricochets(green_bullets,walls,"green")
             handle_ricochets(red_bullets,walls,"red")
             grn_hitbox,red_hitbox = draw_window(green, red, green_bullets, red_bullets, wallbreaker_list,red_angle,green_angle,walls)
-            print(green.x)
-            print(green.y)
     pygame.quit()
 
 if __name__ == "__main__":
